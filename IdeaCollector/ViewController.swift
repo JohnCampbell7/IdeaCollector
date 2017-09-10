03980de7078ae53cea4a37290f744f67b7ee3621
John Campbell <johnc7910@gmail.com>
2017-09-10 21:39:12 +0100

    We can create Idea Objects and pull them out of coredata

--- IdeaCollector/ViewController.swift
+++ IdeaCollector/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ideas : [Idea] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+       ideas = try context.fetch(Idea.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ideas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let idea = ideas[indexPath.row]
+        cell.textLabel?.text = idea.title
+        cell.imageView?.image = UIImage(data: idea.image as! Data)
+        return cell
+        
+    }
 }
 
